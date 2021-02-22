e39e0296345f92c3fcc524cc7871fb1394cf1bb2
KEVISONG <303043149@qq.com>
2021-02-22 22:53:07 +0800

    Add base64 encoding/decoding

--- common/encoding/encoding.go
+++ common/encoding/encoding.go
@@ -1,6 +1,9 @@
 package encoding
 
-import "crypto/rand"
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
 
 // GetRandomString generate random string by specify chars.
 // source: https://github.com/gogits/gogs/blob/9ee80e3e5426821f03a4e99fad34418f5c736413/modules/base/tool.go#L58
@@ -19,4 +22,14 @@ func GetRandomString(n int, alphabets ...byte) (string, error) {
 		}
 	}
 	return string(bytes), nil
+}
+
+// Base64Encode encodes bytes to base64 string
+func Base64Encode(bytes []byte) string {
+	return base64.StdEncoding.EncodeToString(bytes)
+}
+
+// Base64Decode decodes base64 string to bytes
+func Base64Decode(str string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(str)
 }
