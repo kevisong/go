094d5d9ca7a2c57561020b51ea8b62f6802b0ff6
KEVISONG <303043149@qq.com>
2021-04-08 21:27:02 +0800

    Remove random string generator in encoding

--- common/encoding/encoding.go
+++ common/encoding/encoding.go
@@ -1,28 +1,8 @@
 package encoding
 
 import (
-	"crypto/rand"
 	"encoding/base64"
 )
-
-// GetRandomString generate random string by specify chars.
-// source: https://github.com/gogits/gogs/blob/9ee80e3e5426821f03a4e99fad34418f5c736413/modules/base/tool.go#L58
-func GetRandomString(n int, alphabets ...byte) (string, error) {
-	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-	var bytes = make([]byte, n)
-	if _, err := rand.Read(bytes); err != nil {
-		return "", err
-	}
-
-	for i, b := range bytes {
-		if len(alphabets) == 0 {
-			bytes[i] = alphanum[b%byte(len(alphanum))]
-		} else {
-			bytes[i] = alphabets[b%byte(len(alphabets))]
-		}
-	}
-	return string(bytes), nil
-}
 
 // Base64Encode encodes bytes to base64 string
 func Base64Encode(bytes []byte) string {
