086c1a715376be695c6422cb54d6801ae46cb53d
KEVISONG <303043149@qq.com>
2021-04-20 22:37:12 +0800

    Add SHA256

--- common/cryptox/hashing.go
+++ common/cryptox/hashing.go
@@ -2,25 +2,42 @@ package cryptox
 
 import (
 	"crypto/md5"
+	"crypto/sha256"
 	"encoding/hex"
 	"io"
+	"io/ioutil"
 	"strings"
 )
 
-// Md5Sum calculates the md5sum of a stream
-func Md5Sum(reader io.Reader) (string, error) {
-	var returnMD5String string
+// MD5Sum calculates the MD5 of a stream
+func MD5Sum(reader io.Reader) (string, error) {
 	hash := md5.New()
-	if _, err := io.Copy(hash, reader); err != nil {
-		return returnMD5String, err
+	_, err := io.Copy(hash, reader)
+	if err != nil {
+		return "", err
 	}
 	hashInBytes := hash.Sum(nil)[:16]
-	returnMD5String = hex.EncodeToString(hashInBytes)
-	return returnMD5String, nil
+	return hex.EncodeToString(hashInBytes), nil
 }
 
-// Md5SumString calculates the md5sum of a string
-func Md5SumString(input string) (string, error) {
+// MD5SumSumString calculates the MD5 of a string
+func MD5SumSumString(input string) (string, error) {
 	buffer := strings.NewReader(input)
-	return Md5Sum(buffer)
+	return MD5Sum(buffer)
+}
+
+// SHA256 calculates the SHA256 of a stream
+func SHA256(reader io.Reader) (string, error) {
+	data, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return "", err
+	}
+	sum := sha256.Sum256(data)
+	return hex.EncodeToString(sum[:]), nil
+}
+
+// SHA256SumSumString calculates the MD5 of a string
+func SHA256SumSumString(input string) (string, error) {
+	buffer := strings.NewReader(input)
+	return SHA256(buffer)
 }
