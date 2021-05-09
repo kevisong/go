ec84d13bc7255f62935271c15e62a70a4555438b
KEVISONG <303043149@qq.com>
2021-05-09 18:00:15 +0800

    Add mail

--- common/mail/mail.go
+++ common/mail/mail.go
@@ -1,1 +1,33 @@
 package mail
+
+import (
+	gomail "gopkg.in/gomail.v2"
+)
+
+// Sender defines the email sender
+type Sender struct {
+	d *gomail.Dialer
+}
+
+// NewSender factory
+func NewSender(host string, port int, username, password string) *Sender {
+	return &Sender{
+		d: gomail.NewDialer(host, port, username, password),
+	}
+}
+
+// Send sends the email
+func (s *Sender) Send(from, to, subject, body string) error {
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", from)
+	m.SetHeader("To", to)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", body)
+
+	if err := s.d.DialAndSend(m); err != nil {
+		return err
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -14,6 +14,8 @@ require (
 	github.com/sirupsen/logrus v1.7.0
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gorm.io/driver/mysql v1.0.5
 	gorm.io/gorm v1.21.3
 )
