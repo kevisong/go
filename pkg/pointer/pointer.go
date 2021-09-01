8c121282e260e25edaf146674f18e148e3f330be
KEVISONG <303043149@qq.com>
2021-09-01 22:47:55 +0800

    Add comments

--- pkg/pointer/pointer.go
+++ pkg/pointer/pointer.go
@@ -1,33 +1,41 @@
 package pointer
 
+// IntPtr returns *int
 func IntPtr(v int) *int {
 	return &v
 }
 
+// Int64Ptr returns *int64
 func Int64Ptr(v int64) *int64 {
 	return &v
 }
 
+// UintPtr returns *uint
 func UintPtr(v uint) *uint {
 	return &v
 }
 
+// Uint64Ptr returns *uint64
 func Uint64Ptr(v uint64) *uint64 {
 	return &v
 }
 
+// Float64Ptr returns *float64
 func Float64Ptr(v float64) *float64 {
 	return &v
 }
 
+// BoolPtr returns *bool
 func BoolPtr(v bool) *bool {
 	return &v
 }
 
+// StringPtr returns *string
 func StringPtr(v string) *string {
 	return &v
 }
 
+// StringValues returns values of the *string list
 func StringValues(ptrs []*string) []string {
 	values := make([]string, len(ptrs))
 	for i := 0; i < len(ptrs); i++ {
@@ -38,6 +46,7 @@ func StringValues(ptrs []*string) []string {
 	return values
 }
 
+// IntPtrs returns *int list
 func IntPtrs(vals []int) []*int {
 	ptrs := make([]*int, len(vals))
 	for i := 0; i < len(vals); i++ {
@@ -46,6 +55,7 @@ func IntPtrs(vals []int) []*int {
 	return ptrs
 }
 
+// Int64Ptrs returns *int64 list
 func Int64Ptrs(vals []int64) []*int64 {
 	ptrs := make([]*int64, len(vals))
 	for i := 0; i < len(vals); i++ {
@@ -54,6 +64,7 @@ func Int64Ptrs(vals []int64) []*int64 {
 	return ptrs
 }
 
+// UintPtrs returns *uint list
 func UintPtrs(vals []uint) []*uint {
 	ptrs := make([]*uint, len(vals))
 	for i := 0; i < len(vals); i++ {
@@ -62,6 +73,7 @@ func UintPtrs(vals []uint) []*uint {
 	return ptrs
 }
 
+// Uint64Ptrs returns *uint64 list
 func Uint64Ptrs(vals []uint64) []*uint64 {
 	ptrs := make([]*uint64, len(vals))
 	for i := 0; i < len(vals); i++ {
@@ -70,6 +82,7 @@ func Uint64Ptrs(vals []uint64) []*uint64 {
 	return ptrs
 }
 
+// Float64Ptrs returns *float64 list
 func Float64Ptrs(vals []float64) []*float64 {
 	ptrs := make([]*float64, len(vals))
 	for i := 0; i < len(vals); i++ {
@@ -78,6 +91,7 @@ func Float64Ptrs(vals []float64) []*float64 {
 	return ptrs
 }
 
+// BoolPtrs returns *bool list
 func BoolPtrs(vals []bool) []*bool {
 	ptrs := make([]*bool, len(vals))
 	for i := 0; i < len(vals); i++ {
@@ -86,6 +100,7 @@ func BoolPtrs(vals []bool) []*bool {
 	return ptrs
 }
 
+// StringPtrs returns *string list
 func StringPtrs(vals []string) []*string {
 	ptrs := make([]*string, len(vals))
 	for i := 0; i < len(vals); i++ {
