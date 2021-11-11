27861e2d86dfdea516e93b8861955fdf33f3a585
KEVISONG <303043149@qq.com>
2021-11-11 22:47:51 +0800

    feat: add factory pattern

--- internal/design-patterns/creational/factory/factory.go
+++ internal/design-patterns/creational/factory/factory.go
@@ -1,3 +1,61 @@
 package factory
 
-// TODO
+import (
+	"fmt"
+)
+
+type iGun interface {
+	SetName(string)
+	GetName() string
+	SetPower(int)
+	GetPower() int
+}
+
+type Gun struct {
+	Name  string
+	Power int
+}
+
+func (g *Gun) SetName(name string) {
+	g.Name = name
+}
+
+func (g *Gun) GetName() string {
+	return g.Name
+}
+
+func (g *Gun) SetPower(power int) {
+	g.Power = power
+}
+
+func (g *Gun) GetPower() int {
+	return g.Power
+}
+
+type AK struct {
+	Gun
+}
+
+func NewAK() iGun {
+	return &AK{}
+}
+
+type M4 struct {
+	Gun
+}
+
+func NewM4() iGun {
+	return &M4{}
+}
+
+// NewGun is the factory method
+func NewGun(gunType string) (iGun, error) {
+	switch gunType {
+	case "AK":
+		return NewAK(), nil
+	case "M4":
+		return NewM4(), nil
+	default:
+		return nil, fmt.Errorf("gunType %s not supported", gunType)
+	}
+}
