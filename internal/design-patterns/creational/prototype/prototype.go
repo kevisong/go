afc83f7e786b0d829f980528e6a2f896dc1dab27
KEVISONG <303043149@qq.com>
2021-11-22 00:18:16 +0800

    feat:add prototype

--- internal/design-patterns/creational/prototype/prototype.go
+++ internal/design-patterns/creational/prototype/prototype.go
@@ -1,3 +1,22 @@
 package prototype
 
-// TODO
+import "fmt"
+
+// Rectangle is a prototype
+type Rectangle struct {
+	x     int
+	y     int
+	color string
+}
+
+// Clone returns a copy of the prototype
+func (r Rectangle) Clone() Rectangle {
+	return r
+}
+
+func Run() {
+	r1 := Rectangle{x: 0, y: 0, color: "blue"}
+	r2 := r1.Clone()
+	fmt.Println(r1)
+	fmt.Println(r2)
+}
