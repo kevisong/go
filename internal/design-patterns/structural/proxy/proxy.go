fb401240a6ad507a050cd07591f8733e983fbfa5
KEVISONG <303043149@qq.com>
2021-11-29 22:37:00 +0800

    feat: add proxy

--- internal/design-patterns/structural/proxy/proxy.go
+++ internal/design-patterns/structural/proxy/proxy.go
@@ -1,3 +1,44 @@
 package proxy
 
-// TODO
+import (
+	"fmt"
+	"time"
+)
+
+type IUser interface {
+	Login(username, password string) error
+}
+
+type User struct{}
+
+func (u *User) Login(username, password string) error {
+	// Login
+	return nil
+}
+
+type UserProxy struct {
+	user *User
+}
+
+func (u *UserProxy) Login(username, password string) error {
+	// Before
+	start := time.Now()
+
+	err := u.user.Login(username, password)
+	if err != nil {
+		return err
+	}
+
+	// After
+	end := time.Now().Sub(start)
+
+	fmt.Printf("start: %v lasts: %v", start, end)
+	return nil
+}
+
+func Run() {
+	userProxy := UserProxy{
+		user: &User{},
+	}
+	userProxy.Login("username", "password")
+}
