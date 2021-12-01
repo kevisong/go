638017c06c489ce27a933a1c9f6247140d0169ce
KEVISONG <303043149@qq.com>
2021-12-01 22:34:46 +0800

    feat: add bridge

--- internal/design-patterns/structural/bridge/bridge.go
+++ internal/design-patterns/structural/bridge/bridge.go
@@ -1,3 +1,29 @@
 package bridge
 
-// TODO
+type ISender interface {
+	Send(msg string) error
+}
+
+type EmailSender struct{}
+
+func (e *EmailSender) Send(msg string) error {
+	return nil
+}
+
+type INotification interface {
+	Notify(msg string) error
+}
+
+type ErrorNotification struct {
+	sender ISender
+}
+
+func (e *ErrorNotification) Notify(msg string) error {
+	e.sender.Send(msg)
+	return nil
+}
+
+func Run() {
+	errNotification := ErrorNotification{sender: &EmailSender{}}
+	errNotification.Notify("hi")
+}
