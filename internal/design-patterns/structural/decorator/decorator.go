990599aae2dec9551aaf09463697f17b8e292b39
KEVISONG <303043149@qq.com>
2021-12-02 20:43:59 +0800

    feat: add decorator

--- internal/design-patterns/structural/decorator/decorator.go
+++ internal/design-patterns/structural/decorator/decorator.go
@@ -1,3 +1,38 @@
 package decorator
 
-// TODO
+// Use of the decorator can provide enhanced logic.
+
+type IUser interface {
+	Login(username, password string) error
+}
+
+type User struct{}
+
+func (u *User) Login(username, password string) error {
+	// Login
+	return nil
+}
+
+type VIPUser struct {
+	user *User
+}
+
+func (v *VIPUser) showBadge() {}
+
+func (v *VIPUser) Login(username, password string) error {
+	err := v.user.Login(username, password)
+	if err != nil {
+		return err
+	}
+
+	v.showBadge()
+
+	return nil
+}
+
+func Run() {
+	vipUser := VIPUser{
+		user: &User{},
+	}
+	vipUser.Login("username", "password")
+}
