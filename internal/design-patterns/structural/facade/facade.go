83abb4922c6d8ed36df3c622801bfe21de2965d4
KEVISONG <303043149@qq.com>
2021-12-21 00:10:40 +0800

    feat: add facade

--- internal/design-patterns/structural/facade/facade.go
+++ internal/design-patterns/structural/facade/facade.go
@@ -1,3 +1,23 @@
 package facade
 
-// TODO
+type UserService struct{}
+
+func (u *UserService) Login() error {
+	return nil
+}
+
+func (u *UserService) Register() error {
+	return nil
+}
+
+func (u *UserService) LoginOrRegister() error {
+	err := u.Login()
+	if err != nil {
+		return err
+	}
+	err = u.Login()
+	if err != nil {
+		return err
+	}
+	return nil
+}
