06440d01840554071746fa02e03c9fad1cbb8eb0
KEVISONG <303043149@qq.com>
2021-12-22 21:35:37 +0800

    feat: add flyweight

--- internal/design-patterns/structural/flyweight/flyweight.go
+++ internal/design-patterns/structural/flyweight/flyweight.go
@@ -1,3 +1,52 @@
 package flyweight
 
-// TODO
+type ChessPieceUnit struct {
+	ID    uint
+	Name  string
+	Color string
+}
+
+var units = map[int]*ChessPieceUnit{
+	1: {
+		ID:    1,
+		Name:  "車",
+		Color: "red",
+	},
+	2: {
+		ID:    2,
+		Name:  "炮",
+		Color: "red",
+	},
+}
+
+type ChessPiece struct {
+	Unit *ChessPieceUnit
+	X    int
+	Y    int
+}
+
+type ChessBoard struct {
+	chessPieces map[int]*ChessPiece
+}
+
+func NewChessBoard() *ChessBoard {
+	board := &ChessBoard{chessPieces: map[int]*ChessPiece{}}
+	for id := range units {
+		board.chessPieces[id] = &ChessPiece{
+			Unit: units[id],
+			X:    0,
+			Y:    0,
+		}
+	}
+	return board
+}
+
+func (c *ChessBoard) Move(id, x, y int) {
+	c.chessPieces[id].X = x
+	c.chessPieces[id].Y = y
+}
+
+func Run() {
+	board1 := NewChessBoard()
+	board1.Move(1, 1, 2)
+}
